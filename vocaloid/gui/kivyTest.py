4c5837b0e058f9ee77bac1d1ec23dee37e21cd52
Ethan Hong <yihanh@umich.edu>
2018-02-14 02:07:44 -0500

    Rough scaffolding for file selection from base screen

--- vocaloid/gui/kivyTest.py
+++ vocaloid/gui/kivyTest.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 # from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 
 from kivy.uix.filechooser import FileChooserIconView
@@ -18,14 +19,25 @@ class FileChooserPopup(GridLayout):
         self.file_chooser = FileChooserIconView()
         self.add_widget(self.file_chooser)
 
-        self.lyric_button = Button(text='Choose lyric file')
+        self.lyric_button = Button(text='Confirm lyric file')
         self.add_widget(self.lyric_button)
+
+class BaseScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(BaseScreen, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.lyric_popup = Popup(context=FileChooserPopup)
+
+        self.lyric_chooser = Button(text='Choose lyric file', on_press=self.lyric_popup)
+        self.add_widget(self.lyric_chooser)
 
 
 class MyApp(App):
 
     def build(self):
-        return FileChooserPopup()
+        return BaseScreen()
 
 
 if __name__ == '__main__':
