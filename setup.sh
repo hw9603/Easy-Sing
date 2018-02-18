090a3430d78bf85be96f25d031bb5ea35dc084c7
lizihan <lizihan@umich.edu>
2018-02-18 10:19:41 -0500

    refined setup scripts 2

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         # ...
         sudo apt-get -y install libjack-dev
-		sudo apt-get install libasound2-dev
+        sudo apt-get install libasound2-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
         brew install libjack-dev
@@ -11,12 +11,12 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
         echo "Did't tested on Linux on Windows, try at your own risk."
         sudo apt-get -y install libjack-dev
-		sudo apt-get install libasound2-dev
+        sudo apt-get install libasound2-dev
 elif [[ "$OSTYPE" == "msys" ]]; then
         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
         echo "Did't tested on MinGW, try at your own risk."
         sudo apt-get -y install libjack-dev
-		sudo apt-get install libasound2-dev
+        sudo apt-get install libasound2-dev
 else
         echo "known system..."
         exit 1
