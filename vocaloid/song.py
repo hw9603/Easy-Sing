21c8c3a102e2e9aa5cab7558de96ed92f9677666
Alex Mueller <alecastl@umich.edu>
2018-02-19 15:31:06 -0500

    Added class method in Song to convert to MaryXML

--- vocaloid/song.py
+++ vocaloid/song.py
@@ -6,6 +6,25 @@ from vocaloid.phonemesParser import get_phonemes_ssml as get_ssml
 from vocaloid.phonemesParser import get_phonemes
 from vocaloid.syllablesParser import *
 
+def convertToMilliseconds(length):
+    """Convert note length to ms. Currently assumes bpm of 120."""
+    if length.value == 1:
+        return 250
+    if length.value == 2:
+        return 500
+    if length.value == 3:
+        return 1000
+    if length.value == 4:
+        return 2000
+
+def convertPitchToABS(pitch):
+    # pitches ordered according to experimental correspondence data
+    # lowest is C4, highest is B4
+    pitches = [1275, 1285, 1300, 1315,
+               1330, 1350, 1380, 1400,
+               1420, 1440, 1470, 1495]
+    return pitches[pitch.value]
+
 class Song:
     def __init__(self, lyrics):
         self.notes = []
@@ -30,3 +49,39 @@ class Song:
             self.syllables.append(syllable)
         for phonemes in parse_phonemes(get_phonemes(get_ssml(line))):
             self.phonemes.append(phonemes)
+    def convertToMaryXML(self):
+        file = open("song.xml", "w")
+
+        # the first line is really long...
+        line = '<?xml version="1.0" encoding="UTF-8"?>'
+        line = line + '<maryxml xmlns="http://mary.dfki.de/2002/MaryXML"'
+        line = line + ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+        line = line + ' version="0.5" xml:lang="en-US">'
+
+        # write overhead at the beginning
+        file.write(line + "\n")
+        file.write("<p>\n")
+        file.write("<s>\n")
+
+        # for each note, write pitch, length, phonemes
+        # (currently assumes octave is 4 due to lack of data)
+        for note in self.notes:
+            line = '<prosody rate="' + str(convertToMilliseconds(note.length)) + 'ms"'
+            line = line + ' pitch="' + str(convertPitchToABS(note.pitch)) + '">'
+            file.write(line + "\n")
+            line_ph = '<t ph="'
+            for phoneme in note.phonemes:
+                line_ph = line_ph + phoneme
+            line_ph.strip()
+            line_ph = line_ph + '" >'
+            file.write(line_ph + "\n")
+            file.write(note.syllable + "\n")   # this line might not be necessary...
+            file.write("</t>\n")
+            file.write("</prosody>\n")
+        
+        # write overhead at the end
+        file.write("</p>\n")
+        file.write("</s>\n")
+        file.write("</maryxml>\n")
+
+        file.close()
--- vocaloid/test_song.py
+++ vocaloid/test_song.py
@@ -4,57 +4,57 @@ from vocaloid.song import *
 
 def test():
 	lines = [
-		"A lot of words are in this sentence",
-		"I wonder how many words there are",
-		"And how difficult it is to parse",
+		"I made up this song to prove a point",
+		"I wonder how well I make it sound",
+		"And how difficult it is to rhyme",
 		"Based solely on this type of input"
 	]
 
-	# melody from Blue Danube by Strauss, transposed to C
+	# melody from Blue Danube by Strauss, in the key of D Major
 	melody = [
 		[
-			[1, 0, 1],
-			[1, 0, 1],
-			[1, 4, 1],
-			[1, 7, 1],
-			[1, 7, 2],
-			[1, 7, 1],
-			[1, 7, 2],
-			[1, 4, 1],
-			[1, 4, 2]
+			[4, 2, 1],
+			[4, 2, 1],
+			[4, 6, 1],
+			[4, 9, 1],
+			[4, 9, 2],
+			[4, 9, 1],
+			[4, 9, 2],
+			[4, 6, 1],
+			[4, 6, 2]
 		],
 		[
-			[1, 0, 1],
-			[1, 0, 1],
-			[1, 4, 1],
-			[1, 7, 1],
-			[1, 7, 2],
-			[1, 7, 1],
-			[1, 7, 2],
-			[1, 5, 1],
-			[1, 5, 2]
+			[4, 2, 1],
+			[4, 2, 1],
+			[4, 6, 1],
+			[4, 9, 1],
+			[4, 9, 2],
+			[4, 9, 1],
+			[4, 9, 2],
+			[4, 7, 1],
+			[4, 7, 2]
 		],
 		[
-			[0, 11, 1],
-			[0, 11, 1],
-			[1, 2, 1],
-			[1, 9, 1],
-			[1, 9, 2],
-			[1, 9, 1],
-			[1, 9, 2],
-			[1, 5, 1],
-			[1, 5, 2]
+			[4, 1, 1],
+			[4, 1, 1],
+			[4, 4, 1],
+			[4, 7, 1],
+			[4, 11, 2],
+			[4, 11, 1],
+			[4, 11, 2],
+			[4, 7, 1],
+			[4, 7, 2]
 		],
 		[
-			[0, 11, 1],
-			[0, 11, 1],
-			[1, 2, 1],
-			[1, 9, 1],
-			[1, 9, 2],
-			[1, 9, 1],
-			[1, 9, 2],
-			[1, 7, 1],
-			[1, 7, 2]
+			[4, 1, 1],
+			[4, 1, 1],
+			[4, 4, 1],
+			[4, 7, 1],
+			[4, 11, 2],
+			[4, 11, 1],
+			[4, 11, 2],
+			[4, 9, 1],
+			[4, 9, 2]
 		],
 	]
 
@@ -66,4 +66,9 @@ def test():
 		for note in phrase:
 			s.addNote(note[0], note[1], note[2])
 
+	print("----------THIS IS THE SONG------------")
 	print(s)
+
+	s.convertToMaryXML()
+
+test()
