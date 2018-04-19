e540c8ac70a7527b6c94861b2d49b0850934790c
Ethan Hong <yihanh@umich.edu>
2018-04-18 23:59:14 -0400

    revert

--- vocaloid/syllablesParser.py
+++ vocaloid/syllablesParser.py
@@ -11,9 +11,7 @@ Example usage:
     lyrics = "These are some lyrics stored in a string"
     syllables = parse_syllables(lyrics)
 """
-from urllib.request import Request, urlopen
-import json
-import requests
+
 import pyphen
 
 def parse_syllables(lyrics):
@@ -22,23 +20,12 @@ def parse_syllables(lyrics):
     function in pyphen.
     """
     dic = pyphen.Pyphen(lang='en_US')
-
-
-    words = lyrics.split()
+    if dic is not None:
+        words = lyrics.split()
     syllables_list = []
     for word in words:
-        url = "https://wordsapiv1.p.mashape.com/words/" + word
-        headers = {"X-Mashape-Key": "9Bp7D9Gq43mshVEnul9CQfqD05g7p1ZMv72jsnBTH0HWLrHM1B","Accept": "application/json"}
-        response = requests.get(url, headers=headers)
-
-        response = response.json()
-        print(response)
-        try:
-            syllables = response["syllables"]["list"]
-        except:
-            hyphened_word = dic.inserted(word)
-            syllables = hyphened_word.split('-')
-
+        hyphened_word = dic.inserted(word)
+        syllables = hyphened_word.split('-')
         for s in syllables:
             syllables_list.append(s)
     return syllables_list
