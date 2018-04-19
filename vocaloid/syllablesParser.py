2c925f4bc63322b34abad05bc0093e1a89763ce1
hewen <hewen@umich.edu>
2018-04-18 20:05:04 -0400

    use wordsAPI for parsing syllables

--- vocaloid/syllablesParser.py
+++ vocaloid/syllablesParser.py
@@ -11,21 +11,30 @@ Example usage:
     lyrics = "These are some lyrics stored in a string"
     syllables = parse_syllables(lyrics)
 """
-
-import pyphen
+from urllib.request import Request, urlopen
+import json
+import requests
 
 def parse_syllables(lyrics):
     """
     Return a list of syllables. Input words are split into syllables using
     function in pyphen.
     """
-    dic = pyphen.Pyphen(lang='en_US')
-    if dic is not None:
-        words = lyrics.split()
+    # dic = pyphen.Pyphen(lang='en_US')
+
+
+    words = lyrics.split()
     syllables_list = []
     for word in words:
-        hyphened_word = dic.inserted(word)
-        syllables = hyphened_word.split('-')
+        url = "https://wordsapiv1.p.mashape.com/words/" + word
+        headers = {"X-Mashape-Key": "9Bp7D9Gq43mshVEnul9CQfqD05g7p1ZMv72jsnBTH0HWLrHM1B","Accept": "application/json"}
+        response = requests.get(url, headers=headers)
+
+        response = response.json()
+        syllables = response["syllables"]["list"]
+        # hyphened_word = dic.inserted(word)
+        # syllables = hyphened_word.split('-')
+
         for s in syllables:
             syllables_list.append(s)
     return syllables_list
